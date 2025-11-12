6718d1f6b7d9d800aad6bc82a862a654a6b94a8f
thefab04 <karthikvelu0415@gmail.com>
2025-11-12 16:54:20 +0530

    testing3 build

--- backend/server.js
+++ backend/server.js
@@ -120,7 +120,7 @@ app.post("/api/auth/login", [
 
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/*", (req, res) => {
+app.get(/. */, (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
